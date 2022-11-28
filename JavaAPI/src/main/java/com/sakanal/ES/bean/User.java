ddb71a9e14ed6b656fa9e879023908cb5610354b
sakanal <sakanal9527@gmail.com>
2022-11-28 16:22:18 +0800

    commit

---
+++ JavaAPI/src/main/java/com/sakanal/ES/bean/User.java
@@ -0,0 +1,15 @@
+package com.sakanal.ES.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String id;
+    private String name;
+    private String sex;
+    private Integer age;
+}
