94aea2c4bacab73ec5b68b12a1b4573671e05877
sakanal <sakanal9527@gmail.com>
2022-11-29 22:07:32 +0800

    ES7-JavaAPI操作

---
+++ ES/src/test/java/com/sakanal/ES/SearchTests.java
@@ -0,0 +1,57 @@
+package com.sakanal.ES;
+
+import com.sakanal.ES.bean.Product;
+import com.sakanal.ES.dao.ProductDao;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.GetQuery;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchTests {
+
+    @Resource
+    private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @Autowired
+    private ProductDao productDao;
+    /**
+     * term 查询
+     * search(termQueryBuilder) 调用搜索方法，参数查询构建器对象
+     */
+    @Test
+    public void termQuery(){
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("title", "小米");
+        Iterable<Product> products = productDao.search(termQueryBuilder);
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+    /**
+     * term 查询加分页
+     */
+    @Test
+    public void termQueryByPage(){
+        int currentPage= 0 ;
+        int pageSize = 5;
+        //设置查询分页
+        PageRequest pageRequest = PageRequest.of(currentPage, pageSize);
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("title", "小米");
+        Iterable<Product> products =
+                productDao.search(termQueryBuilder,pageRequest);
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+
+}
