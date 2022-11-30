b3969d9b1bc3224217da62847022e21c2b4b065d
sakanal <sakanal9527@gmail.com>
2022-11-30 14:03:25 +0800

    文档搜索

---
+++ JavaAPI8/src/main/java/com/sakanal/api/search/Operation.java
@@ -0,0 +1,30 @@
+package com.sakanal.api.search;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch.core.*;
+import co.elastic.clients.elasticsearch.core.bulk.BulkOperation;
+import co.elastic.clients.elasticsearch.core.bulk.CreateOperation;
+import co.elastic.clients.elasticsearch.core.bulk.DeleteOperation;
+import com.sakanal.api.bean.User;
+import com.sakanal.api.utils.ESClientUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Operation {
+    public static void main(String[] args) throws Exception {
+        //索引客户端
+        ElasticsearchClient client = ESClientUtils.getClient();
+
+        MatchQuery matchQuery = new MatchQuery.Builder().field("age").query(24).build();
+        Query query = new Query.Builder().match(matchQuery).build();
+        SearchRequest searchRequest = new SearchRequest.Builder().query(query).build();
+        SearchResponse<Object> search = client.search(searchRequest, Object.class);
+
+        System.out.println(search);
+
+        ESClientUtils.close();
+    }
+}
---
+++ JavaAPI8/src/main/java/com/sakanal/api/search/OperationLambda.java
@@ -0,0 +1,28 @@
+package com.sakanal.api.search;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.FuzzyQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import com.sakanal.api.utils.ESClientUtils;
+
+public class OperationLambda {
+    public static void main(String[] args) throws Exception {
+        //索引客户端
+        ElasticsearchClient client = ESClientUtils.getClient();
+
+        SearchResponse<Object> searchResponse = client.search(SearchRequest.of(
+                searchRequest -> searchRequest.query(Query.of(
+                        query-> query.fuzzy(FuzzyQuery.of(
+                                fuzzyQuery->fuzzyQuery.field("name").value("李")
+                        ))
+                ))
+        ), Object.class);
+        System.out.println(searchResponse);
+
+
+        ESClientUtils.close();
+    }
+}
