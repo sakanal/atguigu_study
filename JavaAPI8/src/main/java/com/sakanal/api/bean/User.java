03b2632d4eee9283595570dde230b04d5d8cf5ee
sakanal <sakanal9527@gmail.com>
2022-11-30 12:02:32 +0800

    文档类型创建

---
+++ JavaAPI8/src/main/java/com/sakanal/api/bean/User.java
@@ -0,0 +1,16 @@
+package com.sakanal.api.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    private Long id;
+    private String name;
+    private Integer age;
+}
